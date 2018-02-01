b08113320dbbe710767b5f6f51ffe9c98b012046
fuLinHu <1205668006@qq.com>
2018-02-01 10:56:15 +0800

    修啊改

--- guns-generator/src/main/java/com/stylefeng/guns/generator/action/GunsCodeGenerator.java
+++ guns-generator/src/main/java/com/stylefeng/guns/generator/action/GunsCodeGenerator.java
@@ -7,12 +7,12 @@ import com.stylefeng.guns.generator.action.config.GunsGeneratorConfig;
  * 代码生成器,可以生成实体,dao,service,controller,html,js
  *
  * @author stylefeng
- * @Date 2017/5/21 12:38
+ * @Date 2017/5/21 12:3
  */
 public class GunsCodeGenerator {
 
     public static void main(String[] args) {
-
+        //这里需要修改
         /**
          * Mybatis-Plus的代码生成器:
          *      mp的代码生成器可以生成实体,mapper,mapper对应的xml,service
